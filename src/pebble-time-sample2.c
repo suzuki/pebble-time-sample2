de105f43f527cb07503dae0d083c128c78aed42b
Norio Suzuki <norio.suzuki@gmail.com>
2015-06-15 08:46:46 +0900

    Added point_layer.

--- src/pebble-time-sample2.c
+++ src/pebble-time-sample2.c
@@ -2,6 +2,7 @@
 
 static Window *window;
 static TextLayer *text_layer;
+static Layer *point_layer;
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Select");
@@ -21,6 +22,13 @@ static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
+static void point_layer_update_callback(Layer *layer, GContext *ctx) {
+  GPoint p0 = GPoint(0, 0);
+  GPoint p1 = GPoint(100, 100);
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_draw_line(ctx, p0, p1);
+}
+
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
@@ -29,10 +37,16 @@ static void window_load(Window *window) {
   text_layer_set_text(text_layer, "Press a button");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
+
+  point_layer = layer_create(GRect(0, 0, 144, 168));
+  layer_set_update_proc(point_layer, point_layer_update_callback);
+  layer_add_child(window_layer, point_layer);
 }
 
 static void window_unload(Window *window) {
   text_layer_destroy(text_layer);
+
+  layer_destroy(point_layer);
 }
 
 static void init(void) {
