ae332efc58e8235727370b7c673425cbf2cfa1b8
Norio Suzuki <norio.suzuki@gmail.com>
2015-06-16 07:50:58 +0900

    Remove point_layer, rect_layer, image_layer AND added antialias_layer.

--- src/pebble-time-sample2.c
+++ src/pebble-time-sample2.c
@@ -2,10 +2,7 @@
 
 static Window *window;
 static TextLayer *text_layer;
-static Layer *point_layer;
-static Layer *rect_layer;
-static Layer *image_layer;
-static GBitmap *image;
+static Layer *antialias_layer;
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Select");
@@ -25,31 +22,10 @@ static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
-static void point_layer_update_callback(Layer *layer, GContext *ctx) {
-  GPoint p0 = GPoint(0, 0);
-  GPoint p1 = GPoint(100, 100);
-  graphics_context_set_stroke_color(ctx, GColorBlack);
-  graphics_draw_line(ctx, p0, p1);
-}
-
-static void rect_layer_update_callback(Layer *layer, GContext *ctx) {
-  GRect bounds = layer_get_bounds(layer);
-  GRect frame = GRect(5,5, bounds.size.w, 18 + 2);
-
-  graphics_context_set_text_color(ctx, GColorRed);
-  graphics_draw_text(
-    ctx,
-    "Hello World",
-    fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD),
-    frame,
-    GTextOverflowModeTrailingEllipsis,
-    GTextAlignmentCenter,
-    NULL
-  );
-}
-
-static void image_layer_update_callback(Layer *layer, GContext *ctx) {
-  graphics_draw_bitmap_in_rect(ctx, image, layer_get_bounds(layer));
+static void antialias_layer_update_callback(Layer *layer, GContext *ctx) {
+  graphics_draw_circle(ctx, GPoint(35, 35), 30);
+  graphics_context_set_antialiased(ctx, false);
+  graphics_draw_circle(ctx, GPoint(110, 35), 30);
 }
 
 static void window_load(Window *window) {
@@ -61,26 +37,15 @@ static void window_load(Window *window) {
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 
-  point_layer = layer_create(GRect(0, 0, 144, 168));
-  layer_set_update_proc(point_layer, point_layer_update_callback);
-  layer_add_child(window_layer, point_layer);
-
-  rect_layer = layer_create(GRect(0, 0, 144, 168));
-  layer_set_update_proc(rect_layer, rect_layer_update_callback);
-  layer_add_child(window_layer, rect_layer);
-
-  image = gbitmap_create_with_resource(RESOURCE_ID_ICON_IMAGE);
-  image_layer = layer_create(GRect(0, (168 - 60), 60, 60));
-  layer_set_update_proc(image_layer, image_layer_update_callback);
-  layer_add_child(window_layer, image_layer);
+  antialias_layer = layer_create(bounds);
+  layer_set_update_proc(antialias_layer, antialias_layer_update_callback);
+  layer_add_child(window_layer, antialias_layer);
 }
 
 static void window_unload(Window *window) {
   text_layer_destroy(text_layer);
 
-  layer_destroy(point_layer);
-  layer_destroy(rect_layer);
-  layer_destroy(image_layer);
+  layer_destroy(antialias_layer);
 }
 
 static void init(void) {
